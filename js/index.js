9b0672e73f59fd455791577d5da539c8dd8150ca
Sabbir487 <rjsabbir101v@gmail.com>
2024-09-25 22:53:51 +0600

    Full Project Done

--- js/index.js
+++ js/index.js
@@ -17,6 +17,7 @@ document.getElementById('donate-btn').addEventListener('click', function(){
     document.getElementById('money-add').innerText = newDonation;
     appendToHistory(addMoney);
     document.getElementById('modal').showModal();
+    document.getElementById('input-field').value = '';
 
     function appendToHistory(amount) {
     const donationText = document.getElementById('show-this').innerText; // Get the donation title
@@ -34,7 +35,7 @@ document.getElementById('donate-btn').addEventListener('click', function(){
         </div>`;
     
     historyContainer.appendChild(div);
-}
+    }
 })
 // Noakhali flood victims end
 
@@ -57,6 +58,7 @@ document.getElementById('feni-btn').addEventListener('click', function(){
     document.getElementById('feni-money-add').innerText = newDonation;
     appendToHistory(addMoney);
     document.getElementById('modal').showModal();
+    document.getElementById('feni-input-field').value = '';
 
     function appendToHistory(amount) {
     const feniDonation = document.getElementById('feni-show-history').innerText; // Get the donation title
@@ -78,11 +80,53 @@ document.getElementById('feni-btn').addEventListener('click', function(){
 })
 // Feni flood victims end
 
-document.getElementById('history').addEventListener('click',function(){
-    getFooterById('foot');  
+// Qouta protest victims start 
+document.getElementById('qouta-btn').addEventListener('click', function(){
+    const addMoney = getInputFieldById('qouta-input-field');
+    if(isNaN(addMoney) || addMoney < 0){
+      alert('Please put your amount')
+      return;
+    }
+    const moneyMinus = getTextFieldById('availabe-money');
+    const availableMoney = moneyMinus - addMoney;
+    if(addMoney > moneyMinus){
+      alert('You do not have insuficient balance');
+      return;
+    }
+    document.getElementById('availabe-money').innerText = availableMoney;
+     const donationMoney = getTextFieldById('qouta-money-add')
+    const newDonation = donationMoney + addMoney;
+    document.getElementById('qouta-money-add').innerText = newDonation;
+    appendToHistory(addMoney);
+    document.getElementById('modal').showModal();
+    document.getElementById('qouta-input-field').value = '';
+
+    function appendToHistory(amount) {
+    const donationText = document.getElementById('qouta-show-history').innerText; // Get the donation title
+    const historyContainer = document.getElementById('append-here');
+
+    const div = document.createElement("div");
+    div.className = "bg-white p-[25px] rounded-2xl border border-[rgba(17,17,17,0.1)] mb-2";
+    
+    div.innerHTML = `
+        <h4 class="text-xl font-bold text-[rgb(17,17,17)]">
+            You have donated ${amount} BDT for ${donationText}
+        </h4>
+        <div class="text-[16px] font-light text-[rgba(17,17,17,0.7)] mt-4">
+            <span class="text-xl font-bold text-[rgb(17,17,17)]">Date:</span> ${new Date().toLocaleString()}
+        </div>`;
+    
+    historyContainer.appendChild(div);
+}
 })
+// Qouta protest victims end 
+
+document.getElementById('history').addEventListener('click',function(){
+    getFooterById('foot') 
+})
+
 document.getElementById('donation').addEventListener('click',function(){
-    getContainerById('donation-page',);  
+    getContainerById('donation-page'); 
 })
 document.getElementById('history').addEventListener('click',function(){
     getContainerById('history-page'); 
