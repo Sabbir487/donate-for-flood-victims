0aae267536e31bf287e91e40d5b10826c09983fd
Sabbir487 <rjsabbir101v@gmail.com>
2024-09-25 22:01:06 +0600

    Blog Page Fully Completed

--- js/index.js
+++ js/index.js
@@ -38,6 +38,46 @@ document.getElementById('donate-btn').addEventListener('click', function(){
 })
 // Noakhali flood victims end
 
+// Feni flood victims start
+document.getElementById('feni-btn').addEventListener('click', function(){
+    const addMoney = getInputFieldById('feni-input-field');
+    if(isNaN(addMoney) || addMoney < 0){
+      alert('Please put your amount')
+      return;
+    }
+    const moneyMinus = getTextFieldById('availabe-money');
+    const availableMoney = moneyMinus - addMoney;
+    if(addMoney > moneyMinus){
+      alert('You do not have insuficient balance');
+      return;
+    }
+    document.getElementById('availabe-money').innerText = availableMoney;
+     const donationMoney = getTextFieldById('feni-money-add')
+    const newDonation = donationMoney + addMoney;
+    document.getElementById('feni-money-add').innerText = newDonation;
+    appendToHistory(addMoney);
+    document.getElementById('modal').showModal();
+
+    function appendToHistory(amount) {
+    const feniDonation = document.getElementById('feni-show-history').innerText; // Get the donation title
+    const historyContainer = document.getElementById('append-here');
+
+    const div = document.createElement("div");
+    div.className = "bg-white p-[25px] rounded-2xl border border-[rgba(17,17,17,0.1)] mb-2";
+    
+    div.innerHTML = `
+        <h4 class="text-xl font-bold text-[rgb(17,17,17)]">
+            You have donated ${amount} BDT for ${feniDonation}
+        </h4>
+        <div class="text-[16px] font-light text-[rgba(17,17,17,0.7)] mt-4">
+            <span class="text-xl font-bold text-[rgb(17,17,17)]">Date:</span> ${new Date().toLocaleString()}
+        </div>`;
+    
+    historyContainer.appendChild(div);
+}
+})
+// Feni flood victims end
+
 document.getElementById('history').addEventListener('click',function(){
     getFooterById('foot');  
 })
